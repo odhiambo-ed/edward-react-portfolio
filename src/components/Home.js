627421850ab8dff0752ebf393debd7dbd6ccb38d
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-25 20:40:42 +0300

    Edit Home CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,51 @@
 import React from 'react'
 import "./Home.css"
 import Profile from "../images/Passport_Edward.png"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Home = () => {
     return (
         <div style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "flex-start",
-            flexDirection: "row",
             backgroundColor: "#000"
         }}>
-            <div>
-                <div id="console"> </div>
-            </div>
-            <div
-                id="image"
-                style={{
-                    height: "300px",
-                    width: "300px",
-                    marginTop: "150px"
-                }}
-            >
-                <img
-                    className="img-responsive"
-                    src={Profile}
-                    style={{
-                        height: "100%",
-                        width: "100%"
-                    }}
-                />
-            </div>
-        </div >
+            <Container>
+                <Row>
+                    <Col sm={8}>
+                        <div>
+                            <div id="console"> </div>
+                        </div>
+                    </Col>
+                    <Col style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }} sm={4}>
+                        <div
+                            id="image"
+                            style={{
+                                height: "300px",
+                                width: "300px",
+                                marginTop: "150px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <img
+                                className="img-responsive"
+                                src={Profile}
+                                style={{
+                                    height: "100%",
+                                    width: "100%"
+                                }}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
