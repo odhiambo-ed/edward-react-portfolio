e5f1bd5c64abf955d98e5c775be5e24c0ac2ad0f
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-26 12:52:50 +0300

    Add Material UI

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+
+const TypewriterHook = () => {
+  const { text } = useTypewriter({
+    words: [" a FullStack Web Developer", " an expert in CSS", " an expert in HTML", " an expert in SASS", " an expert in LESS", " an expert in JavaScript", " an expert in Ruby", " an expert in Ruby on Rails", " also a Cyber Security Enthusiast"],
+    loop: 2,
+    onLoopDone: () => console.log('done from typewriter hook')
+  })
+
+  return (
+    <div>
+      <p>
+        I'm
+        <span>{text}</span>
+      </p>
+      <Cursor />
+    </div>
+  )
+}
 
 const Hero = () => {
+
   return (
     <div className="hero-container" data-aos="fade-in">
       <h1>
         <span className="text-danger">Edward</span> Odhiambo
       </h1>
-      <p>
-        I'm
-        <span
-          class="txt-rotate"
-          data-period="2000"
-          data-rotate='[ " a FullStack Web Developer,", " an expert in CSS,", " an expert in HTML,", " an expert in SASS,", " an expert in LESS,", " an expert in JavaScript,", " an expert in Ruby,", " an expert in Ruby on Rails,", " also a Cyber Security Enthusiast" ]'
-        ></span>
-      </p>
+      <TypewriterHook />
     </div>
   );
 };
