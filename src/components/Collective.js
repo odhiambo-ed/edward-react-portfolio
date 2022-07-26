474c4c6d2179d17e5eda2d2bcd45e58835079c99
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-26 12:54:35 +0300

    Final Touches

--- src/components/Collective.js
+++ src/components/Collective.js
@@ -10,11 +10,84 @@ import Portfolio from "./Portfolio";
 import Services from "./Services";
 import Testimonials from "./Testimonials";
 import Contact from "./Contact";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const Collective = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        if (anchorEl) {
+            setAnchorEl(null);
+        }
+        else {
+            setAnchorEl(event.currentTarget);
+        }
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <div>
-            <i className="bi bi-list mobile-nav-toggle d-xl-none" />
+            <i
+                id="demo-positioned-button"
+                aria-controls={open ? 'demo-positioned-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+                className="bi bi-list mobile-nav-toggle d-xl-none"
+            />
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <a href="#hero" className="nav-link scrollto active">
+                        <i className="bx bx-home" /> <span>Home</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="#about" className="nav-link scrollto">
+                        <i className="bx bx-user" /> <span>About</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="#resume" className="nav-link scrollto">
+                        <i className="bx bx-file-blank" /> <span>Resume</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="#portfolio" className="nav-link scrollto">
+                        <i className="bx bx-book-content" /> <span>Portfolio</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="#services" className="nav-link scrollto">
+                        <i className="bx bx-server" /> <span>Services</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="https://medium.com/me/stories/public" className="nav-link scrollto">
+                        <i className="bx bx-server" /> <span>Blog</span>
+                    </a>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <a href="#contact" className="nav-link scrollto">
+                        <i className="bx bx-envelope" /> <span>Contact</span>
+                    </a>
+                </MenuItem>
+            </Menu>
             <header id="header">
                 <Header />
             </header>
