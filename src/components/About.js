b6440d619dc28bf8b63a3b3a85aafe254ceb1e09
white3d <odhiambo.edward111@gmail.com>
2022-11-30 16:10:29 +0300

    Edit About Component

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,13 @@ const About = () => {
       <div className="section-title">
         <h2>About Me</h2>
         <p>
-          Incredibly Talented Full-Stack Software Developer with a love for
-          Cyber security Solutions such as Network Firewalls, Internet Traffic
-          Protection Systems, Protected Remote Access, Security Event Monitoring
-          Systems and Intrusion Prevention Systems. A wealth of experience in
-          directing teams in driving Software, Hardware and IT centered
-          projects. Seeks to bring mastery and aptitude to the team. Excited for
-          new ventures and discoveries.
+          Talented and innovative Full-Stack Web Developer with over 4 years of hands-on
+          experience in web application development. Focused and efficient learner with
+          advanced proficiency in HTML, CSS, JavaScript, Ruby, PostgreSQL and excellent
+          customer service skills. I also have a solid foundation in the following
+          frameworks: Bootstrap, ReactJS, Redux, Node.JS, Ruby on Rails and Test-driven development (Jest and RSpec).
+          Seeking to leverage broad development experience and hands-on technical
+          expertise in a challenging role as a Full-stack Developer.
         </p>
       </div>
       <div className="row">
@@ -20,15 +20,16 @@ const About = () => {
           <img src="assets/img/Edward2.jpeg" className="img-fluid" alt="prof" />
         </div>
         <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-          <h3>Software Engineering &amp; Cyber Security Specialist</h3>
+          <h3>SOFTWARE ENGINEER | Full Stack Developer(Rails, ReactJS)</h3>
           <p className="fst-italic">
-            A developer with a love for
+            A developer with a love for Front-End:
             <mark>HTML</mark>,<mark>CSS</mark>,<mark>JavaScript</mark>,
-            <mark>React</mark>,<mark>Ruby</mark>and
-            <mark>Ruby on Rails</mark> / A Cyber security enthusiast with
-            top-notch abilities in Cyber Security Solutions; Network Firewalls,
-            Internet Traffic Protection Systems, Protected Remote Access,
-            Security Event Monitoring Systems and Intrusion Prevention Systems.
+            <mark>React</mark>,<mark>Redux</mark>,<mark>JQuery</mark>,<mark>HTML5</mark>,
+            <mark>CSS3</mark>,<mark>Bootstrap5</mark>,<mark>Semantic UI</mark>and
+            <mark>Material UI</mark>. Back-End: <mark>Ruby</mark>, <mark>Rails</mark>, <mark>PostgreSQL</mark>. 
+            Tools & Methods: <mark>Git</mark>,<mark>GitHub</mark>,<mark>Heroku</mark>,<mark>Netlify</mark>,<mark>Mobile Development</mark>,
+            <mark>TDD (Jest/RSpec)</mark> and <mark>Chrome Dev Tools</mark>. Professional: <mark>Remote Pair-Programming</mark>,<mark>Teamwork</mark>
+            and <mark>Mentoring</mark>
           </p>
           <div className="row">
             <div className="col-lg-6">
